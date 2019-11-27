3c59642470a783000d3da049a6f7c913b7c69365
Connor Lee <45024235+connorleee@users.noreply.github.com>
2019-11-26 18:55:19 -0800

    superbalanced problem

---
+++ superbalanced.js
@@ -0,0 +1,144 @@
+class BinaryTreeNode {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+
+    insertLeft(value) {
+        this.left = new BinaryTreeNode(value);
+        return this.left;
+    }
+
+    insertRight(value) {
+        this.right = new BinaryTreeNode(value);
+        return this.right;
+    }
+}
+
+function isBalanced(treeRoot) {
+
+    // Determine if the tree is superbalanced
+    if (!treeRoot) {
+        return true
+    }
+
+    // console.log(treeRoot)
+
+    let depths = [];
+
+    // List of nodes that we've popped off and want to observe. this is a stack 
+    let nodes = [];
+
+    // We are creating node pairs. nodePair[0] is the treeNode, and nodePair[1] is a count of depth.
+    nodes.push([treeRoot, 0]);
+
+    while (nodes.length) {
+        let nodePair = nodes.pop();
+        let node = nodePair[0];
+        let depth = nodePair[1];
+
+        // This is a leaf node
+        if (!node.left && !node.right) {
+            if (depths.indexOf(depth) < 0) {
+                depths.push(depth);
+
+                if (depths.length > 2 || (depths.length === 2 && Math.abs(depths[0] - depths[1]) > 1)) {
+                    return false
+                }
+            }
+
+        // If we're not at a leaf node, keep traversing
+        } else {
+            if (node.left) {
+                nodes.push([node.left, depth + 1]);
+            }
+
+            if (node.right) {
+                nodes.push([node.right, depth + 1])
+            }
+        }
+    }
+
+return true;
+}
+
+
+
+
+
+
+// Tests
+
+let desc = 'full tree';
+let treeRoot = new BinaryTreeNode(5);
+let leftNode = treeRoot.insertLeft(8);
+leftNode.insertLeft(1);
+leftNode.insertRight(2);
+let rightNode = treeRoot.insertRight(6);
+rightNode.insertLeft(3);
+rightNode.insertRight(4);
+assertEquals(isBalanced(treeRoot), true, desc);
+
+desc = 'both leaves at the same depth';
+treeRoot = new BinaryTreeNode(3);
+leftNode = treeRoot.insertLeft(4);
+leftNode.insertLeft(1);
+rightNode = treeRoot.insertRight(6);
+rightNode.insertRight(9);
+assertEquals(isBalanced(treeRoot), true, desc);
+
+desc = 'leaf heights differ by one';
+treeRoot = new BinaryTreeNode(6);
+leftNode = treeRoot.insertLeft(1);
+rightNode = treeRoot.insertRight(0);
+rightNode.insertRight(7);
+assertEquals(isBalanced(treeRoot), true, desc);
+
+desc = 'leaf heights differ by two';
+treeRoot = new BinaryTreeNode(6);
+leftNode = treeRoot.insertLeft(1);
+rightNode = treeRoot.insertRight(0);
+rightNode.insertRight(7).insertRight(8);
+assertEquals(isBalanced(treeRoot), false, desc);
+
+desc = 'three leaves total';
+treeRoot = new BinaryTreeNode(1);
+leftNode = treeRoot.insertLeft(5);
+rightNode = treeRoot.insertRight(9);
+rightNode.insertLeft(8);
+rightNode.insertRight(5);
+assertEquals(isBalanced(treeRoot), true, desc);
+
+desc = 'both subtrees superbalanced';
+treeRoot = new BinaryTreeNode(1);
+leftNode = treeRoot.insertLeft(5);
+rightNode = treeRoot.insertRight(9);
+rightNode.insertLeft(8).insertLeft(7);
+rightNode.insertRight(5);
+assertEquals(isBalanced(treeRoot), false, desc);
+
+desc = 'both subtrees superbalanced two';
+treeRoot = new BinaryTreeNode(1);
+leftNode = treeRoot.insertLeft(2);
+leftNode.insertLeft(3);
+leftNode.insertRight(7).insertRight(8);
+treeRoot.insertRight(4).insertRight(5).insertRight(6).insertRight(9);
+assertEquals(isBalanced(treeRoot), false, desc);
+
+desc = 'only one node';
+treeRoot = new BinaryTreeNode(1);
+assertEquals(isBalanced(treeRoot), true, desc);
+
+desc = 'linked list tree';
+treeRoot = new BinaryTreeNode(1);
+treeRoot.insertRight(2).insertRight(3).insertRight(4);
+assertEquals(isBalanced(treeRoot), true, desc);
+
+function assertEquals(a, b, desc) {
+    if (a === b) {
+        console.log(`${desc} ... PASS`);
+    } else {
+        console.log(`${desc} ... FAIL: ${a} != ${b}`)
+    }
+}
