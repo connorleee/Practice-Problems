1829e50731103fc32436b7bc1f6cb0c771780f24
Connor Lee <45024235+connorleee@users.noreply.github.com>
2020-01-03 20:40:50 -0800

    fixed off by 1 error

--- is-first-come-first-serve.js
+++ is-first-come-first-serve.js
@@ -1,66 +1,73 @@
 function isFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders) {
     //confirm all orders are entered
-    if(servedOrders.length != takeOutOrders.length + dineInOrders.length){
-        return false;
-    }
-
     let outOrderPointer = 0;
     let inOrderPointer = 0;
+    let outOrderMaxPointer = takeOutOrders.length - 1;
+    let inOrderMaxPointer = dineInOrders.length - 1;
 
     for (let i = 0; i < servedOrders.length; i++) {
         const orderUp = servedOrders[i];
-        
-        if(orderUp != takeOutOrders[outOrderPointer] || orderUp != dineInOrders[inOrderPointer]) {
+
+        // if (orderUp != takeOutOrders[outOrderPointer] || orderUp != dineInOrders[inOrderPointer]) {
+        //     return false;
+        // }
+
+        if (outOrderPointer <= outOrderMaxPointer && orderUp === takeOutOrders[outOrderPointer]) {
+            outOrderPointer++;
+        } else if (inOrderPointer <= inOrderMaxPointer && orderUp === dineInOrders[inOrderPointer]) {
+            inOrderPointer++;
+        } else {
             return false;
         }
-
-        orderUp === takeOutOrders[outOrderPointer] ? outOrderPointer++ : inOrderPointer++;
     }
-    
-  
+
+    if(outOrderPointer != takeOutOrders.length || inOrderPointer != dineInOrders.length){
+        return false;
+    }
+
     return true;
-  }
-  
-  
-  
-  
-  
+}
 
-  
-  // Tests
-  
-  let desc = 'both registers have same number of orders';
-  let actual = isFirstComeFirstServed([1, 4, 5], [2, 3, 6], [1, 2, 3, 4, 5, 6]);
-  assertEquals(actual, true, desc);
-  
-  desc = 'registers have different lengths';
-  actual = isFirstComeFirstServed([1, 5], [2, 3, 6], [1, 2, 6, 3, 5]);
-  assertEquals(actual, false, desc);
-  
-  desc = 'one register is empty';
-  actual = isFirstComeFirstServed([], [2, 3, 6], [2, 3, 6]);
-  assertEquals(actual, true, desc);
-  
-  desc = 'served orders is missing orders';
-  actual = isFirstComeFirstServed([1, 5], [2, 3, 6], [1, 6, 3, 5]);
-  assertEquals(actual, false, desc);
-  
-  desc = 'served orders has extra orders';
-  actual = isFirstComeFirstServed([1, 5], [2, 3, 6], [1, 2, 3, 5, 6, 8]);
-  assertEquals(actual, false, desc);
-  
-  desc = 'one register has extra orders';
-  actual = isFirstComeFirstServed([1, 9], [7, 8], [1, 7, 8]);
-  assertEquals(actual, false, desc);
-  
-  desc = 'one register has unserved orders';
-  actual = isFirstComeFirstServed([55, 9], [7, 8], [1, 7, 8, 9]);
-  assertEquals(actual, false, desc);
-  
-  function assertEquals(a, b, desc) {
+
+
+
+
+
+
+// Tests
+
+let desc = 'both registers have same number of orders';
+let actual = isFirstComeFirstServed([1, 4, 5], [2, 3, 6], [1, 2, 3, 4, 5, 6]);
+assertEquals(actual, true, desc);
+
+desc = 'registers have different lengths';
+actual = isFirstComeFirstServed([1, 5], [2, 3, 6], [1, 2, 6, 3, 5]);
+assertEquals(actual, false, desc);
+
+desc = 'one register is empty';
+actual = isFirstComeFirstServed([], [2, 3, 6], [2, 3, 6]);
+assertEquals(actual, true, desc);
+
+desc = 'served orders is missing orders';
+actual = isFirstComeFirstServed([1, 5], [2, 3, 6], [1, 6, 3, 5]);
+assertEquals(actual, false, desc);
+
+desc = 'served orders has extra orders';
+actual = isFirstComeFirstServed([1, 5], [2, 3, 6], [1, 2, 3, 5, 6, 8]);
+assertEquals(actual, false, desc);
+
+desc = 'one register has extra orders';
+actual = isFirstComeFirstServed([1, 9], [7, 8], [1, 7, 8]);
+assertEquals(actual, false, desc);
+
+desc = 'one register has unserved orders';
+actual = isFirstComeFirstServed([55, 9], [7, 8], [1, 7, 8, 9]);
+assertEquals(actual, false, desc);
+
+function assertEquals(a, b, desc) {
     if (a === b) {
-      console.log(`${desc} ... PASS`);
+        console.log(`${desc} ... PASS`);
     } else {
-      console.log(`${desc} ... FAIL: ${a} != ${b}`);
+        console.log(`${desc} ... FAIL: ${a} != ${b}`);
     }
-  }
+}
