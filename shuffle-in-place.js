c99a7c815906d754e1d0abd2974fe76f249ef1bc
Connor Lee <45024235+connorleee@users.noreply.github.com>
2020-01-06 19:08:07 -0800

    shuff in place done

--- shuffle-in-place.js
+++ shuffle-in-place.js
@@ -1,16 +1,25 @@
 function getRandom(floor, ceiling) {
     return Math.floor(Math.random() * (ceiling - floor + 1)) + floor;
-  }
-  
-  function shuffle(array) {
-  
+}
+
+function shuffle(array) {
+
     // Shuffle the input in place
-    
-  
-  }
-  
-  
-  const sample = [1, 2, 3, 4, 5];
-  console.log('Initial array: ', sample);
-  shuffle(sample);
-  console.log('Shuffled array: ', sample);
+    if (array.length <= 1) return;
+
+    for (let idxChoosingFor = 0; idxChoosingFor < array.length; idxChoosingFor++) {
+        const randChoiceIdx = getRandom(idxChoosingFor, array.length - 1);
+
+        if(idxChoosingFor !== randChoiceIdx){
+            const randValChosen = array[randChoiceIdx];
+            array[randChoiceIdx] = array[idxChoosingFor];
+            array[idxChoosingFor] = randValChosen;
+        }
+    }
+}
+
+
+const sample = [1, 2, 3, 4, 5];
+console.log('Initial array: ', sample);
+shuffle(sample);
+console.log('Shuffled array: ', sample);
