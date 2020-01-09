d533bcdfecd6b7075820621a57770b00254be504
Connor Lee <45024235+connorleee@users.noreply.github.com>
2020-01-08 21:55:36 -0800

    added recursive solution

--- is-binary-tree.js
+++ is-binary-tree.js
@@ -57,6 +57,21 @@ function isBinarySearchTree(treeRoot) {
     return true;
 }
 
+// // recursive
+// function isBinarySearchTree(treeRoot, lowerBound, upperBound){
+//     lowerBound = (typeof lowerBound !== "undefined") ? lowerBound : Number.NEGATIVE_INFINITY;
+//     upperBound = (typeof upperBound !== "undefined") ? upperBound : Number.POSITIVE_INFINITY;
+
+//     if ( !treeRoot ){
+//         return true;
+//     }
+
+//     if(treeRoot.value > upperBound || treeRoot.value < lowerBound) {
+//         return false;
+//     }
+
+//     return isBinarySearchTree(treeRoot.left, lowerBound, treeRoot.value) && isBinarySearchTree(treeRoot.right, treeRoot.value, upperBound);
+// }
 
 
 
