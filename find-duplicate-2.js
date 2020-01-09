37955da178add90a2a8e702e0676c9b7de218209
Connor Lee <45024235+connorleee@users.noreply.github.com>
2020-01-08 19:58:02 -0800

    find duplicate done

--- find-duplicate-2.js
+++ find-duplicate-2.js
@@ -1,56 +1,75 @@
 function findRepeat(numbers) {
 
     // Find a number that appears more than once
-    numbers.sort((a, b) => {return a-b})
+    numbers.sort((a, b) => { return a - b })
 
-    console.log(numbers)
-  
-    return 0;
-  }
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  // Tests
-  
-  let desc = 'just the repeated number';
-  let actual = findRepeat([1, 1]);
-  let expected = 1;
-  assertEqual(actual, expected, desc);
-  
-  desc = 'short array';
-  actual = findRepeat([1, 2, 3, 2]);
-  expected = 2;
-  assertEqual(actual, expected, desc);
-  
-  desc = 'medium array';
-  actual = findRepeat([1, 2, 5, 5, 5, 5]);
-  expected = 5;
-  assertEqual(actual, expected, desc);
-  
-  desc = 'long array';
-  actual = findRepeat([4, 1, 4, 8, 3, 2, 7, 6, 5]);
-  expected = 4;
-  assertEqual(actual, expected, desc);
-  
-  function assertEqual(a, b, desc) {
-    if (a === b) {
-      console.log(`${desc} ... PASS`);
-    } else {
-      console.log(`${desc} ... FAIL: ${a} != ${b}`);
+    for (let i = 0; i < numbers.length; i++) {
+        const el = numbers[i];
+        if (el === numbers[i + 1]) {
+            return el;
+        }
     }
-  }
+
+    return 0;
+}
+
+// // Using a set - O(n) time and space
+// function findRepeat(numbers) {
+//     const numbersSeen = new Set();
+
+//     for (let i = 0; i < numbers.length; i++) {
+//         const el = numbers[i];
+        
+//         if(numbersSeen.has(el)){
+//             return el;
+//         }
+
+//         numbersSeen.add(el);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Tests
+
+let desc = 'just the repeated number';
+let actual = findRepeat([1, 1]);
+let expected = 1;
+assertEqual(actual, expected, desc);
+
+desc = 'short array';
+actual = findRepeat([1, 2, 3, 2]);
+expected = 2;
+assertEqual(actual, expected, desc);
+
+desc = 'medium array';
+actual = findRepeat([1, 2, 5, 5, 5, 5]);
+expected = 5;
+assertEqual(actual, expected, desc);
+
+desc = 'long array';
+actual = findRepeat([4, 1, 4, 8, 3, 2, 7, 6, 5]);
+expected = 4;
+assertEqual(actual, expected, desc);
+
+function assertEqual(a, b, desc) {
+    if (a === b) {
+        console.log(`${desc} ... PASS`);
+    } else {
+        console.log(`${desc} ... FAIL: ${a} != ${b}`);
+    }
+}
