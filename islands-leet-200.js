f585ec2e3bf090ba0d2039904a87b8e4e747f5a8
Connor Lee <45024235+connorleee@users.noreply.github.com>
2020-01-10 20:58:46 -0800

    started islands question from leet code

---
+++ islands-leet-200.js
@@ -0,0 +1,16 @@
+var numIslands = function(grid) {
+    const islands = new Set();
+    
+    let islandCount = 0;
+    
+    for(let y = 0; y < grid.length; y++) {
+        for(let x = 0; x < grid[y].length) { 
+            
+            if(grid[y][x] === 1){
+                let     
+            }
+        }
+    }
+    
+    return islandCount;
+};
--- mesh-message.js
+++ mesh-message.js
@@ -1,47 +1,70 @@
 class Queue {
     constructor() {
-      this.queue = [];
-      this.size = 0;
+        this.queue = [];
+        this.size = 0;
     }
-  
+
     enqueue(item) {
-      this.queue.unshift(item);
-      this.size += 1;
+        this.queue.unshift(item);
+        this.size += 1;
     }
-  
+
     dequeue() {
-      this.size -= 1;
-      return this.queue.pop();
+        this.size -= 1;
+        return this.queue.pop();
     }
-  }
-  
-  function getPath(graph, startNode, endNode) {
-  
+}
+
+function getPath(graph, startNode, endNode) {
+
     // Find the shortest route in the network between the two users
-    
-  
+
+    const queue = new Queue();
+    queue.enqueue([startNode, 0]);
+
+    // keep track of array and distance from og
+    const nodesSeen = new Set([startNode]);
+
+    while (queue.size > 0) {
+        const nodePair = queue.dequeue();
+        const node = nodePair[0];
+        const distanceFromStart = nodePair[1];
+
+        if (node === endNode) {
+            // found the end
+            return distanceFromStart
+        }
+
+        graph[node].forEach(neighbor => {
+            if (!nodesSeen.has(neighbor)) {
+                nodesSeen.add(neighbor);
+                queue.enqueue([neighbor, distanceFromStart + 1]);
+            }
+        })
+    }
+
     return null;
-  }
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  // Tests
-  const graph = {
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Tests
+const graph = {
     'a': ['b', 'c', 'd'],
     'b': ['a', 'd'],
     'c': ['a', 'e'],
@@ -49,68 +72,68 @@ class Queue {
     'e': ['c'],
     'f': ['g'],
     'g': ['f']
-  };
-  
-  let desc = 'two hop path 1';
-  let actual = getPath(graph, 'a', 'e');
-  let expected = ['a', 'c', 'e'];
-  assertDeepEqual(actual, expected, desc);
-  
-  desc = 'two hop path 2';
-  actual = getPath(graph, 'd', 'c');
-  expected = ['d', 'a', 'c'];
-  assertDeepEqual(actual, expected, desc);
-  
-  desc = 'one hop path 1';
-  actual = getPath(graph, 'a', 'c');
-  expected = ['a', 'c'];
-  assertDeepEqual(actual, expected, desc);
-  
-  desc = 'one hop path 2';
-  actual = getPath(graph, 'f', 'g');
-  expected = ['f', 'g'];
-  assertDeepEqual(actual, expected, desc);
-  
-  desc = 'one hop path 3';
-  actual = getPath(graph, 'g', 'f');
-  expected = ['g', 'f'];
-  assertDeepEqual(actual, expected, desc);
-  
-  desc = 'zero hop path';
-  actual = getPath(graph, 'a', 'a');
-  expected = ['a'];
-  assertDeepEqual(actual, expected, desc);
-  
-  desc = 'no path';
-  actual = getPath(graph, 'a', 'f');
-  expected = null;
-  assertDeepEqual(actual, expected, desc);
-  
-  desc = 'start node not present';
-  assertThrowsError(() => {
+};
+
+let desc = 'two hop path 1';
+let actual = getPath(graph, 'a', 'e');
+let expected = ['a', 'c', 'e'];
+assertDeepEqual(actual, expected, desc);
+
+desc = 'two hop path 2';
+actual = getPath(graph, 'd', 'c');
+expected = ['d', 'a', 'c'];
+assertDeepEqual(actual, expected, desc);
+
+desc = 'one hop path 1';
+actual = getPath(graph, 'a', 'c');
+expected = ['a', 'c'];
+assertDeepEqual(actual, expected, desc);
+
+desc = 'one hop path 2';
+actual = getPath(graph, 'f', 'g');
+expected = ['f', 'g'];
+assertDeepEqual(actual, expected, desc);
+
+desc = 'one hop path 3';
+actual = getPath(graph, 'g', 'f');
+expected = ['g', 'f'];
+assertDeepEqual(actual, expected, desc);
+
+desc = 'zero hop path';
+actual = getPath(graph, 'a', 'a');
+expected = ['a'];
+assertDeepEqual(actual, expected, desc);
+
+desc = 'no path';
+actual = getPath(graph, 'a', 'f');
+expected = null;
+assertDeepEqual(actual, expected, desc);
+
+desc = 'start node not present';
+assertThrowsError(() => {
     getPath(graph, 'h', 'a');
-  }, desc);
-  
-  desc = 'end node not present';
-  assertThrowsError(() => {
+}, desc);
+
+desc = 'end node not present';
+assertThrowsError(() => {
     getPath(graph, 'a', 'h');
-  }, desc);
-  
-  function assertDeepEqual(a, b, desc) {
+}, desc);
+
+function assertDeepEqual(a, b, desc) {
     const aStr = JSON.stringify(a);
     const bStr = JSON.stringify(b);
     if (aStr !== bStr) {
-      console.log(`${desc} ... FAIL: ${aStr} != ${bStr}`);
+        console.log(`${desc} ... FAIL: ${aStr} != ${bStr}`);
     } else {
-      console.log(`${desc} ... PASS`);
+        console.log(`${desc} ... PASS`);
     }
-  }
-  
-  function assertThrowsError(func, desc) {
+}
+
+function assertThrowsError(func, desc) {
     try {
-      func();
-      console.log(`${desc} ... FAIL`);
+        func();
+        console.log(`${desc} ... FAIL`);
     } catch (e) {
-      console.log(`${desc} ... PASS`);
+        console.log(`${desc} ... PASS`);
     }
-  }
+}
