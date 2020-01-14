9ddb790d10b86607a368a21f348f769a48082423
Connor Lee <45024235+connorleee@users.noreply.github.com>
2020-01-13 19:57:41 -0800

    integer to english started. initialized arrays of english words

---
+++ integer-to-english.js
@@ -0,0 +1,6 @@
+var numberToWords = function (num) {
+    const ones = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    const teens = ["", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    const tens = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+    const suffixes = ["", "hundred", "thousand", "million", "billion", "trillion"];
+}
