9a36a053e5960633add7978ed82f0e9ae39e9478
Connor Lee <45024235+connorleee@users.noreply.github.com>
2020-01-14 20:37:23 -0800

    started leet 2 add two numbers

---
+++ add-two-numbers-leet-2.js
@@ -0,0 +1,24 @@
+let addTwoNumbers = function(l1, l2) {
+    let currentNode = new ListNode(0);
+
+    let sum = 0;
+
+    while(l1 || l2) {
+        if(l1) {
+            sum += l1.val;
+            l1 = l1.next;
+        }
+
+        if(l2) {
+            sum += l2.val;
+            l2 = l2.next;
+        }
+
+        currentNode.next = new ListNode(sum % 10);
+        currentNode = currentNode.next;
+
+        sum = sum >= 10 ? 1 : 0;
+    }
+
+    return currentNode.next();
+}
