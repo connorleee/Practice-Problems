eae359fbc62080cb8b4009933ceecc43f8949166
Connor Lee <45024235+connorleee@users.noreply.github.com>
2020-01-15 20:43:31 -0800

    at least able to complete the first test. fails on other...

--- reorder-data-logs-leet-937.js
+++ reorder-data-logs-leet-937.js
@@ -6,34 +6,41 @@ let reorderLogFiles = function (logs) {
 
     let results = [];
 
+    const body = s => s.slice(s.indexOf(' ') + 1); // get the body after identifier
+
+    // if body same then compare identifier
+    const compare = (a, b) => {
+        const n = body(a).localeCompare(body(b));
+        if (n !== 0) return n;
+        return a.localeCompare(b);
+    };
+
     // split each element of logs do traverse easier for identification
     for (let i = 0; i < logs.length; i++) {
         const el = logs[i];
         logsSplit.push(el.split(" "));
     }
-    
+
     // filter the logs into one of two buckets
     for (let j = 0; j < logsSplit.length; j++) {
         const el = logsSplit[j];
-        
-        console.log(typeof el)
 
         // if we have a digit sequence
-        if(parseInt(el[1])) {
+        if (parseInt(el[1])) {
             digits.push(el.join(" ")); //ready to push straight into the results
         } else {
-            letters.push(el); //will need to join later
+            letters.push(el.join(" ")); //will need to join later
         }
     }
 
     // need to sort the words
-    letters.sort((a, b) => a[1].localeCompare(b[1]));
+    // letters.sort((a, b) => a[1].localeCompare(b[1]));
 
-    results.push(...letters);
+    results.push(...letters.sort(compare));
     results.push(...digits);
 
     return results;
 };
 
-console.log(reorderLogFiles(["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"])
+console.log(reorderLogFiles(["dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero"])
 )
