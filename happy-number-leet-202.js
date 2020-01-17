1a54e12f528f55c0070106592973354a477c787a
Connor Lee <45024235+connorleee@users.noreply.github.com>
2020-01-16 20:26:02 -0800

    happy-number-leet-202 started

---
+++ happy-number-leet-202.js
@@ -0,0 +1,28 @@
+let isHappy = function(n) {
+    function sumOfSquares(int) {
+        let intArray = (""+int).split(""); //splits integer into array of digits. turns into strings
+        
+        let sum = 0;
+
+        for(let i = 0; i < intArray.length; i++){
+            const digit = parseInt(intArray[i]);
+            sum += el * el;
+        }
+
+        return sum;
+    }
+
+    if(n === 1) return true;
+
+    let numsSeen = new Set();
+
+    while( n !== 1 ) {
+        if(numsSeen.has(n)) return false;
+
+        numsSeen.add(n);
+
+        n = sumOfSquares(n);
+    }
+    
+    return true;
+}
