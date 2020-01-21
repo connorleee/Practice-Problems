8d580c477742f8a358070ca54ab961d6afe8df8b
Connor Lee <45024235+connorleee@users.noreply.github.com>
2020-01-20 21:06:25 -0800

    finished LRU cache. note that Map objects keep track of insertion order

--- LRU-Cache-leet-146.js
+++ LRU-Cache-leet-146.js
@@ -1,28 +1,24 @@
 /**
  * @param {number} capacity
  */
-var LRUCache = function(capacity) {
-    const capacity = capacity;
-    let cache = [];
+var LRUCache = function (capacity) {
+    this.cap = capacity;
+    this.cache = new Map();
 };
 
 /** 
  * @param {number} key
  * @return {number}
  */
-LRUCache.prototype.get = function(key) {
-    
-    for(let i = 0; i < this.cache.length; i++) { 
-        const el = this.cache[i];
+LRUCache.prototype.get = function (key) {
+    let val = this.cache.get(key);
 
-        if(el[0] === key) {
-            const temp = this.cache.splice[i,1]; //remove the cache from the queue so it can be pushed to the top of the queue
-            this.cache.push(temp); //move cache to end of queue
-            return this.cache[temp];
-        }
-    }
+    if(typeof val === "undefined") return -1
 
-    return -1
+    this.cache.delete(key); // resetting the order of the key value pair taking advantage of the map insertion order
+    this.cache.set(key, val);
+
+    return val;
 };
 
 /** 
@@ -30,34 +26,16 @@ LRUCache.prototype.get = function(key) {
  * @param {number} value
  * @return {void}
  */
-LRUCache.prototype.put = function(key, value) {
-    for (let i = 0; i < this.cache.length; i++) {
-        const el = this.cache[i];
-        
-        // first check if key exists and we can just update value
-        if(el[0] === key) {
-            el[1] = value; //update new value
-            const temp = this.cache.splice[1,0]; //since cache was touched, move to end of queue
-            this.cache.push(temp);
-            return
-        }
-
-        if(i === this.cache.length-1 && this.cache.length === this.capacity) {
-            this.cache.shift();
-            this.cache.push([key,value]);
-            return
-        }
+LRUCache.prototype.put = function (key, value) {
+    this.cache.delete(key); //returns false if no key is found
+    this.cache.set(key, value);
+    const keys = this.cache.keys();
+    if( this.cache.size > this.cap) {
+        this.cache.delete(keys.next().value); // deletes the oldest key value pair in the map
     }
-
-    if(this.cache.length < this.capacity) {
-        this.cache.push([key,value]);
-        return
-    }
-
-    throw new Error("something wrong with the put function")
 };
 
-/** 
+/**
  * Your LRUCache object will be instantiated and called as such:
  * var obj = new LRUCache(capacity)
  * var param_1 = obj.get(key)
