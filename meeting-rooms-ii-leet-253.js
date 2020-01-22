72214b655825f1f2274d0f3b3fb8b3c54e10a609
Connor Lee <45024235+connorleee@users.noreply.github.com>
2020-01-21 19:34:35 -0800

    first solution attempt. using min heap

--- meeting-rooms-ii-leet-253.js
+++ meeting-rooms-ii-leet-253.js
@@ -2,6 +2,31 @@
  * @param {number[][]} intervals
  * @return {number}
  */
-const minMeetingRooms = function(intervals) {
-    
+const minMeetingRooms = function (intervals) {
+    if (intervals.length < 1) return 0;
+
+    let meetingEndTimes = [];
+
+    for (let i = 0; i < intervals.length; i++) {
+        const [start, end] = intervals[i];
+        const earliestEnd = meetingEndTimes[0];
+
+        if (!meetingEndTimes.length) {
+            meetingEndTimes.push(end);
+        } 
+
+        // need new room
+        else if (start < earliestEnd) {
+            meetingEndTimes.push(end);
+            meetingEndTimes.sort((a, b) => a - b);
+        }
+
+        // can use a freed up room
+        else {
+            earliestEnd[0] = Math.max(end, earliestEnd[0]);
+            meetingEndTimes.sort((a, b) => a - b);
+        }
+    }
+
+    return meetingEndTimes.length;
 };
