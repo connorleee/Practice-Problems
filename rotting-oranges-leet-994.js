35465ce9aae31721465bf15f0f8db7a56fc12e36
Connor Lee <45024235+connorleee@users.noreply.github.com>
2020-01-22 17:41:07 -0800

    rotting oranges in progress. need to test and work edge cases

--- rotting-oranges-leet-994.js
+++ rotting-oranges-leet-994.js
@@ -8,7 +8,6 @@ const orangesRotting = function(grid) {
 
     const foundRotten = (row, col) => {
         queue.push([row, col]);
-        depth++;
         grid[row][col] = 0;
     }
 
@@ -18,16 +17,37 @@ const orangesRotting = function(grid) {
 
             // found the first rotten orange
             if(el === 2) {
-                queue.push(el);
+                queue.push([row, col]);
                 depth++;
                 grid[row][col] = 0; //any item touched will be turned back to 0 so we don't touch it again.
             }
 
             while(queue.length) {
-                // check right
-                if(grid[row][col + 1] === 2) {
+                const current = queue.shift();
+                const y = current[0];
+                const x = current[1]
 
-                } 
+                // check right
+                if( grid[y][x + 1] === 1 ) {
+                    foundRotten(x + 1, y);
+                }
+                
+                // check left
+                if( grid[y][x-1] === 1) {
+                    foundRotten(x-1, y);
+                }
+
+                // check down
+                if( grid[y + 1][x] === 1) {
+                    foundRotten(y+1, x);
+                }
+
+                // check up
+                if( grid[y - 1][x] === 1) {
+                    foundRotten(y-1, x);
+                }
+
+                depth++
             }
         }
     }
