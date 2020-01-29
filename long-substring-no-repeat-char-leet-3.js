9dfbf755109778acba2793847896a770c3cbe003
Connor Lee <45024235+connorleee@users.noreply.github.com>
2020-01-28 16:40:07 -0800

    longest substring complete

--- long-substring-no-repeat-char-leet-3.js
+++ long-substring-no-repeat-char-leet-3.js
@@ -2,6 +2,24 @@
  * @param {string} s
  * @return {number}
  */
-const lengthOfLongestSubstring = function(s) {
-    
+const lengthOfLongestSubstring = function (s) {
+    if (s.length === 0) return 0
+
+    let max = 0;
+    let currentLetters = new Map(); // (letter, recent index)
+
+    for (let i = 0, j = 0; i < s.length; i++) {
+        const el = s[i];
+
+        if (currentLetters.has(el)) {
+            j = Math.max(currentLetters.get(el), j);
+        }
+
+        max = Math.max(max, i - j + 1);
+        currentLetters.set(el, i + 1);
+    }
+
+    return max;
 };
+
+console.log(lengthOfLongestSubstring("dvdf"))
