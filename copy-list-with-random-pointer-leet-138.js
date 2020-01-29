a1b6ffb96b3e10c99ae2c3620648177b0e93ed79
Connor Lee <45024235+connorleee@users.noreply.github.com>
2020-01-29 14:23:04 -0800

    copy linked list complete

--- copy-list-with-random-pointer-leet-138.js
+++ copy-list-with-random-pointer-leet-138.js
@@ -11,5 +11,28 @@
  * @return {Node}
  */
 var copyRandomList = function(head) {
-    
+    if(!head) return null;
+
+    const map = new Map();
+
+    //populate hash map
+    let cur = head;
+    while(cur !== null) {
+        map.set(cur, new Node(cur.val));
+
+        cur = cur.next;
+    }
+
+    cur = head;
+    while(cur !== null) {
+        // map.set(map.get(cur).next, map.get(cur.next));
+        // map.set(map.get(cur).random, map.get(cur.random));
+
+        map.get(cur).next = map.get(cur.next) || null;
+        map.get(cur).random = map.get(cur.random) || null;
+
+        cur = cur.next;
+    }
+
+    return map.get(head);
 };
