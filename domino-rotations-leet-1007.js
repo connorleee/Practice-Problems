fed31e884ff90236fd5737fb93eeb16ffbf87616
Connor Lee <45024235+connorleee@users.noreply.github.com>
2020-01-28 22:22:22 -0800

    working on edge case where all dominoes have at least one matching number, but not consistently matching

--- domino-rotations-leet-1007.js
+++ domino-rotations-leet-1007.js
@@ -12,17 +12,24 @@ const minDominoRotations = function (A, B) {
 
     for (let i = 1; i < n; i++) {
         // if current dominoe doesn't match either of the options of the first dominoe, flipping isn't possible
-        if((A[i] !== a || A[i] !== b) && (B[i] !== a || B[i] !== b)) {
-            return -1;
+        if(A[i] !== a && A[i] !== b) {
+            if(B[i] !== a && B[i] !== b){
+                return -1;
+            }
         }
 
-        flips++;
+        if(A[i] === a || B[i] === b) {
+            continue;
+        } else if(A[i] === b || B[i] === a) {
+            flips++;
+        } else {
+            return -1
+        }
     }
-
-    
 
     return Math.min(flips, n - flips);
 };
 
 console.log(minDominoRotations([2,1,2,4,2,2], [5,2,6,2,3,2]));
 console.log(minDominoRotations([3,5,1,2,3], [3,6,3,3,4]))
+console.log(minDominoRotations([2,1,1,3,2,1,2,2,1], [3,2,3,1,3,2,3,3,2]))
