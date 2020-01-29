0e06dadf66484afd1dacab5849f309c9bed6d7c2
Connor Lee <45024235+connorleee@users.noreply.github.com>
2020-01-28 22:07:21 -0800

    started domino rotation leet 1007

--- best-time-to-sell-leet-121.js
+++ best-time-to-sell-leet-121.js
@@ -3,5 +3,21 @@
  * @return {number}
  */
 const maxProfit = function(prices) {
+    let max = 0;
+    let buy = prices[0];
     
+    for (let i = 0; i < prices.length; i++) {
+        const price = prices[i];
+        
+        if(price < buy) {
+            buy = price;
+        } else {
+            max = Math.max(max, price - buy);
+        }
+    }
+
+    return max;
 };
+
+console.log(maxProfit([7,1,5,3,6,4]))
+console.log(maxProfit([7,6,4,3,1]))
---
+++ domino-rotations-leet-1007.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} A
+ * @param {number[]} B
+ * @return {number}
+ */
+const minDominoRotations = function(A, B) {
+    
+};
