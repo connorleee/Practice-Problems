f9f377eab44f53c9802058e64a2ec18ee4fc082a
Connor Lee <45024235+connorleee@users.noreply.github.com>
2020-01-30 15:40:12 -0800

    sorting solution for O(NlogN)

--- merge-time-intervals-leet-56.js
+++ merge-time-intervals-leet-56.js
@@ -3,6 +3,25 @@
  * @return {number[][]}
  */
 var merge = function(intervals) {
-    let s1 = intevals[0][0];
-    let e1 = intervals[0][1];
+    //sort by start time
+    intervals.sort((a, b) => {
+        return a[0] - b[0];
+    })
+
+    let result = [[intervals[0]]];
+
+    for (let i = 0; i < intervals.length; i++) {
+        let prevMerge = result[result.length - 1]; //last meeting merged in result to compare
+        let [s2, e2] = intervals[i];
+        
+        if(s2 <= prevMerge[1]) {
+            prevMerge[1] = Math.max(prevMerge[1], e2);
+        } else {
+            result.push([s2, e2])
+        }
+    }
+
+    return result.slice(1);
 };
+
+console.log(merge([[1,3],[2,6],[8,10],[15,18]]))
