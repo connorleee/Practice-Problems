a4403a07b74acdb6862666e20b6d97d00df28f26
Connor Lee <45024235+connorleee@users.noreply.github.com>
2020-01-31 13:18:40 -0800

    solution attempt 1 on game of life

---
+++ game-of-life-leet-289.js
@@ -0,0 +1,55 @@
+/**
+ * @param {number[][]} board
+ * @return {void} Do not return anything, modify board in-place instead.
+ */
+var gameOfLife = function (board) {
+    let neighbors = [0, 1, -1]; //travesal tool
+    let rows = board.length;
+    let cols = board[0].length;
+
+    let copyBoard = [];
+
+    // populate the copied board
+    for (let row = 0; row < rows; row++) {
+        for (let col = 0; col < cols; col++) {
+            copyBoard[row][col] = board[row][col];
+        }
+    }
+
+    // loop through each item and find all live neighbors at each cell. if a 1, apply rules 1-3 logic. if a 0, apply rule 4
+    for (let row = 0; row < rows; row++) {
+        for (let col = 0; col < cols; col++) {
+            let neighborCount = 0;
+
+            // check adjacency cells
+            for (let i = 0; i < neighbors.length; i++) {
+                for (let j = 0; j < neighbors.length; j++) {
+
+                    // this will be used for validity check of adjacent cells.
+                    if (!(neighbors[i] === 0 && neigbors[j] === 0) && copyBoard[r][c] === 1) {
+                        let r = row + neighbors[i];
+                        let c = col + neighbors[j];
+
+                        //actual validity check
+                        if ((r < rows && r >= 0) && (c < cols && c >= 0)) {
+                            neighborCount++;
+                        }
+                    }
+                }
+            }
+
+            // check rules 1 or 3. rule 2 does nothing so we'll check all other cases and ignore if nothing else is met
+            if (copyBoard[row][col] === 1) {
+                // rule 1 or 3
+                if (neighborCount < 2 || neighborCount > 3) {
+                    board[row][col] = 0;
+                }
+            }
+
+            //check rule 4
+            if(copyBoard[row][col] === 0 && neighborCount === 3) {
+                board[row][col] = 1;
+            }
+        }
+    }
+};
--- subarray-sum-to-k-leet-560.js
+++ subarray-sum-to-k-leet-560.js
@@ -4,5 +4,27 @@
  * @return {number}
  */
 var subarraySum = function(nums, k) {
+    let map = new Map(); //unique sum, number of occurences of sum
+    let count = 0;
+    let sum = 0;
 
+    map.set(0, 1); //sum of zero is always present
+
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i]; //new sum
+
+        if(map.has(sum - k)) {
+            count += map.get(sum - k)
+        }
+        if(map.has(sum)) {
+            map.set(sum, map.get(sum) + 1)
+        }
+        else {
+            map.set(sum, 1)
+        }
+    }
+
+    return count;
 };
+
+console.log(subarraySum([1,1,1], 2)); //2
