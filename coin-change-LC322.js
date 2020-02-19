39e546edf0f5f82d637c6829c21d3c56d67e37ce
Connor Lee <45024235+connorleee@users.noreply.github.com>
2020-02-18 19:45:16 -0800

    first attempt at coin change

--- coin-change-LC322.js
+++ coin-change-LC322.js
@@ -4,5 +4,21 @@
  * @return {number}
  */
 var coinChange = function(coins, amount) {
+    coins.sort((a,b) => a - b);
+
+    let total = 0;
+    let count = 0;
+
+    if(coins[0] > amount) return -1;
     
+    for(let i = coins.length - 1; i >= 0; i--) {
+        while(total + coins[i] < amount) {
+            total += coins[i];
+            count++;
+        }
+    }
+
+    if(total !== amount) return -1;
+
+    return count;
 };
