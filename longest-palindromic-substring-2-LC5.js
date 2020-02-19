a672ff2a894a496054e24618d7f5746eedfa39f8
Connor Lee <45024235+connorleee@users.noreply.github.com>
2020-02-18 19:00:04 -0800

    second round on the longest palindrome done

--- longest-palindromic-substring-2-LC5.js
+++ longest-palindromic-substring-2-LC5.js
@@ -2,19 +2,21 @@ const longestPalindrome = function (s) {
     let longest = "";
 
     for(let i = 0; i < s.length; i++) {
-        for(let j = 0; j < 2; j++) {
+        for(let j = 0; j < 2; j++) { //this loop of 0 and 1 are to cover the case where there is an even length palindrome
             let left = i;
             let right = i + j;
 
-            while(left >= 0 && right <= s.length && s[left] === s[right]) {
+            while(left >= 0 && right < s.length && s[left] === s[right]) {
                 left--;
                 right++;
             }
 
-            if(longest < right - left) {
-                longest = s.substring(left, right);
+            if(longest < right - left - 1) {
+                longest = s.substring(left + 1, right);
             }
         }
+
+        if(Math.ceil(longest.length / 2) >= s.length - i) break;
     }
 
     return longest;
