8e25be6da5c856e74afa6bbf5cbc85b6afce777a
Connor Lee <45024235+connorleee@users.noreply.github.com>
2020-02-19 21:54:09 -0800

    attempt at dynamic programming solution to coin problem. no go. stack overflow somewhere

--- coin-change-LC322.js
+++ coin-change-LC322.js
@@ -1,28 +1,35 @@
 /**
+ if(amount === 0) return 0;
  * @param {number[]} coins
  * @param {number} amount
  * @return {number}
  */
-var coinChange = function(coins, amount) {
-    coins.sort((a,b) => a - b);
+var coinChange = function (coins, amount) {
+    if (coins[0] > amount) return -1;
+    coins.sort((a, b) => b - a); //sorts coins decending
+    let minCoins = Infinity;
 
-    let total = 0;
-    let count = 0;
+    function combo(amount, index, count) {
+        if (index >= coins.length) return;
 
-    if(amount === 0) return 0;
-    if(coins[0] > amount) return -1;
-    
-    for(let i = coins.length - 1; i >= 0; i--) {
-        while(total + coins[i] <= amount) {
-            total += coins[i];
-            count++;
+        for (let i = Math.floor(amount / coins[index]); i >= 0; i--) { //starts the loop at the maximum number of times the current coin can be used.
+            let newCount = count + i;
+            let remainder = amount - coins[index] * i;
+
+            if (remainder > 0 && newCount < minCoins) { //recursively call combo function if there is a remainder. Short circuit if we are already over the current minimum number of coins.
+                combo(remainder, index + i, newCount);
+            } else if (newCount < minCoins) {
+                minCoins = newCount;
+            } else if (newCount <= minCoins - 1) { //Short circuit if we are already over the current minimum number of coins.
+                break;
+            }
         }
     }
 
-    if(total !== amount) return -1;
+    combo(amount, 0, 0);
 
-    return count;
+    return minCoins == Infinity ? -1 : minCoins;
 };
 
-console.log(coinChange([1,2,5], 11))
-console.log(coinChange([1,2,5], 0))
+console.log(coinChange([1, 2, 5], 11))
+console.log(coinChange([1, 2, 5], 0))
